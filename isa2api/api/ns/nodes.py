9107b5d228ab752d36d0e5152bcccf88170444de
Zaid ALHADAD <zaid172009@gmail.com>
2017-08-18 11:55:09 +0200

    nodes

--- isa2api/api/ns/nodes.py
+++ isa2api/api/ns/nodes.py
@@ -1,33 +1,51 @@
 import logging
-from flask import make_response,abort
-from flask_restplus import Resource
+from flask import make_response, abort
+from flask_restplus import Resource, reqparse
 from isa2api.api.serializers import *
-from isa2api.api.parsers import login_arguments,token_arguments
+from isa2api.api.parsers import login_arguments, token_arguments
 from isa2api.api.restplus import api
-from isa2api.business.users import Users 
+from isa2api.business.nodes import Nodes
 log = logging.getLogger(__name__)
 
 
+ns = api.namespace('nodes', description='nodes names & labels')
+parser = reqparse.RequestParser()
+parser.add_argument('server', type=int, required=True,
+                    help='Server id should be in Integer')
 
-ns = api.namespace('nodes', description='sessions related informations')
+parser_label = parser.copy()
+parser_label.add_argument(
+    'label', type=str, required=True, help='Should be a string')
 
-user = Users()
-@ns.route("/me")
+
+@ns.route("/")
 @ns.expect(token_arguments)
-class Me(Resource):
+class NodesCtrl(Resource):
     """
-     decode token then send user
+     send all nodes from this service
     """
-    @api.response(200, 'valid token and not expired')
+    @ns.expect(parser)
     def get(self):
-        return user.decodeToken() 
+        args = parser.parse_args()
+        n = Nodes(args)
+        return n.get()
 
-@ns.route('/authenticate')
-class Auth(Resource):
-    @api.expect(login_arguments)
-    def post(self):
-        args = login_arguments.parse_args()
-        token = user.login(args)
-        if not token:
-            return make_response("{'message': 'username / password error'}",403)
-        return token
+
+@ns.route("/<name>")
+class NodeCtrl(Resource):
+    """
+    update node name
+    """
+
+    @ns.expect(parser_label)
+    def put(self, name):
+        args = parser_label.parse_args()
+        args.update({"name": name})
+        n = Nodes(args)
+        return n.update_node(args)
+
+    @ns.expect(parser)
+    def delete(self, name):
+        args = parser.parse_args()
+        n = Nodes(args)
+        return n.delete_node_label({"name": name})
---
+++ isa2api/business/nodes.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import json
+
+from isa2api.database import db, where, select
+
+
+class Nodes():
+
+    def __init__(self, params):
+        r = db.execute("select service from servers where id=%(id)s",
+                       {"id": params.get('server')})
+        self.service = r.scalar()
+
+    def get(self):
+        sql = "SELECT name,label FROM nodes where service = %(service)s ORDER BY name"
+        return select(sql, {"service": self.service})
+
+    def __repr__(self):
+        return None
+
+    def update_node(self, params):
+        sql = "update nodes set label=%(label)s where service = %(service)s and name = %(name)s"
+        params.update({"service": self.service})
+        db.execute(sql, params)
+        sql = "SELECT name,label FROM nodes where service = %(service)s and name = %(name)s"
+        return select(sql, params)
+
+    def delete_node_label(self, params):
+        print(params)
+        sql = "update nodes set label='' where service = %(service)s and name = %(name)s"
+        params.update({"service": self.service})
+        db.execute(sql, params)
+        sql = "SELECT name,label FROM nodes where service = %(service)s and name = %(name)s"
+        return select(sql, params)
